72af6eeb483909a157907fe11eea5a5be294a19c
Edza <edexdev@gmail.com>
2018-12-02 14:25:07 +0200

    Converted into module, still errors

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
-#include <stdio.h>
-int main()
-{
-   char str[20] = "0183456789";
+#include <webassembly.h>
+
+export int add(int x1, int x2) {
+  char str[20] = "0183456789";
   
-   printf("0 elements ir %c \n", *(str + 1));
-   printf("0 elementa adrese %d\n" , str);
-   printf("0 elementa adrese %d\n" , &*(str + 0));
+   //printf("0 elements ir %c \n", *(str + 1));
+   //printf("0 elementa adrese %d\n" , str);
+   //printf("0 elementa adrese %d\n" , &*(str + 0));
 
    char* b = str;
 
-   printf("pointeris uz str %d\n", b);
+   //printf("pointeris uz str %d\n", b);
 
-   printf("atmiņas adresi kurā stāv pointeris uz str %d\n", &b);
+   //printf("atmiņas adresi kurā stāv pointeris uz str %d\n", &b);
 
    char** c = &b;
 
-   printf("pointeris uz b %d\n", c);
+   //printf("pointeris uz b %d\n", c);
 
    char*** d = &c;
 
-   printf("str[2] izteikts tikai ar d %c\n", *((**d) + 2));
+   //printf("str[2] izteikts tikai ar d %c\n", *((**d) + 2));
 
-   return 0;
+   return *((**d) + 2);
 }
